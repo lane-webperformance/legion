53bbaa854b102d4876a70b282239a16799c5530d
Christopher Lane Hinson <lane@webperformance.com>
2016-06-01 09:07:18 -0400

    Adding some extra-defensive programming to try to catch more errors.

--- src/withConcurrency.js
+++ src/withConcurrency.js
@@ -1,15 +1,16 @@
 var Io = require('legion-io');
+var reportingErrors = require('./reportingErrors');
 
 module.exports = function(concurrency, testcase) {
-  testcase = Io.of().chain(testcase);
-
-  return Io.get().chain(function(receiver) {
+  return reportingErrors(Io.get().chain(function(receiver) {
     var user_number = 0;
     var results = [];
+    testcase = Io.of().chain(testcase);
 
     for( user_number = 0; user_number < concurrency; user_number++ )
       results.push(testcase.run(receiver));
 
     return Promise.all(results);
-  });
+  }));
 };
+
